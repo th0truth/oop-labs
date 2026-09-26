7418455b6ba04a7b8383b4fc9adc99406d29de05
th0truth <shiftrams@gmail.com>
2026-09-26 16:58:11 +0300

    feat(task3): add func that computes factorial

--- lab01/internal/tasks/task3.go
+++ lab01/internal/tasks/task3.go
@@ -1,7 +1,53 @@
 package tasks
 
-import "fmt"
+import (
+	"fmt"
+
+	"lab01/internal/input"
+)
+
+func factorialRecursion(n int64) int64 {
+	if n == 0 {
+		return 1
+	} else {
+		return n * factorialRecursion(n-1)
+	}
+}
+
+func factorialIterative(n int64) (int64, int64) {
+	var factorial, sum int64
+	factorial, sum = 1, 0
+
+	if n == 0 {
+		return factorial, sum
+	} else {
+		for i := int64(1); i <= n; i++ {
+			factorial *= i
+			sum += factorial
+		}
+	}
+
+	return factorial, sum
+}
 
 func Task3() {
-	fmt.Println("[task3 not done yet]")
+	fmt.Println("\n--- Task 3: Factorial and sum of factorials ---")
+
+	n := int64(input.Int("Enter n: "))
+
+	fmt.Println("\n  i  |         i!          |    sum of 1!..i!")
+	fmt.Println("-----------------------------------------------")
+
+	var sum int64 = 0
+	for i := int64(1); i <= n; i++ {
+		f := factorialRecursion(i)
+		sum += f
+
+		fmt.Printf("%3d | %19d | %19d\n", i, f, sum)
+	}
+	fmt.Println("-----------------------------------------------")
+
+	fact, total := factorialIterative(int64(n))
+	fmt.Printf("Factorial of %d = %d\n", n, fact)
+	fmt.Printf("Sum of all factorials 1!..%d! = %d\n", n, total)
 }
